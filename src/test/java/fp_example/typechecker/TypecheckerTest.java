e3f8127f3efbbae152c4d7243540861ada97f3dc
Kyle Dewey <kyle.dewey@csun.edu>
2022-04-16 14:14:32 -0700

    tests for first line of expressions

--- src/test/java/fp_example/typechecker/TypecheckerTest.java
+++ src/test/java/fp_example/typechecker/TypecheckerTest.java
@@ -20,16 +20,17 @@ public class TypecheckerTest {
                                         final Map<Variable, TypeTerm> typeEnvironment,
                                         final Exp exp) throws TypeErrorException {
         final Unifier unifier = new Unifier();
-        final TypeTerm receivedType =
-            emptyTypechecker().typeofExp(exp, typeEnvironment, unifier);
+        TypeTerm receivedType = emptyTypechecker().typeofExp(exp,
+                                                             typeEnvironment,
+                                                             unifier);
+        receivedType = unifier.transitiveSetRepresentativeFor(receivedType);
         if (expectedType != null) {
             final TypeTerm expectedTypeAsTerm =
                 Typechecker.translateType(expectedType,
                                           new HashMap<Typevar, TypeTerm>());
             unifier.unify(expectedTypeAsTerm, receivedType);
         } else {
-            fail("Expected exception to be thrown; received type: " +
-                 unifier.transitiveSetRepresentativeFor(receivedType));
+            fail("Expected exception to be thrown; received type: " + receivedType);
         }
     }
 
@@ -42,5 +43,67 @@ public class TypecheckerTest {
     public void testIntLiteralExp() throws TypeErrorException {
         assertExpHasType(new IntType(),
                          new IntLiteralExp(0));
+    }
+
+    @Test
+    public void testVariableInScope() throws TypeErrorException {
+        final Map<Variable, TypeTerm> typeEnvironment =
+            new HashMap<Variable, TypeTerm>();
+        typeEnvironment.put(new Variable("x"), new IntTypeTerm());
+        assertExpHasType(new IntType(),
+                         typeEnvironment,
+                         new VariableExp(new Variable("x")));
+    }
+
+    @Test(expected = TypeErrorException.class)
+    public void testVariableNotInScope() throws TypeErrorException {
+        assertExpHasType(null,
+                         new VariableExp(new Variable("x")));
+    }
+
+    @Test
+    public void testBooleanLiteralExp() throws TypeErrorException {
+        assertExpHasType(new BoolType(),
+                         new BooleanLiteralExp(true));
+    }
+
+    @Test
+    public void testPlusExp() throws TypeErrorException {
+        assertExpHasType(new IntType(),
+                         new OpExp(new IntLiteralExp(1),
+                                   new PlusOp(),
+                                   new IntLiteralExp(2)));
+    }
+
+    @Test
+    public void testLessThanExp() throws TypeErrorException {
+        assertExpHasType(new BoolType(),
+                         new OpExp(new IntLiteralExp(1),
+                                   new LessThanOp(),
+                                   new IntLiteralExp(2)));
+    }
+
+    @Test
+    public void testEqualsExpInts() throws TypeErrorException {
+        assertExpHasType(new BoolType(),
+                         new OpExp(new IntLiteralExp(1),
+                                   new EqualsOp(),
+                                   new IntLiteralExp(2)));
+    }
+
+    @Test
+    public void testEqualsExpBools() throws TypeErrorException {
+        assertExpHasType(new BoolType(),
+                         new OpExp(new BooleanLiteralExp(true),
+                                   new EqualsOp(),
+                                   new BooleanLiteralExp(false)));
+    }
+
+    @Test(expected = TypeErrorException.class)
+    public void testEqualsExpIntBool() throws TypeErrorException {
+        assertExpHasType(null,
+                         new OpExp(new IntLiteralExp(0),
+                                   new EqualsOp(),
+                                   new BooleanLiteralExp(true)));
     }
 }
